89ca0821697b62f8349b3d3b4ce2310d55999e3a
hataehyeok <105369662+hataehyeok@users.noreply.github.com>
2023-09-07 12:21:46 +0900

    pascal

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,14 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if ((r == 0) || (r == 1))
+      1
+    else if ((c == 0) || (c == r))
+      1
+    else
+      pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
