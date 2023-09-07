5e39079937a7a5c0c94ba34ddde9144c82a1deb2
hataehyeok <105369662+hataehyeok@users.noreply.github.com>
2023-09-07 11:25:29 +0900

    Define sum and max func

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -23,7 +23,12 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    if (xs.isEmpty)
+      0
+    else
+      xs.head + sum(xs.tail)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +43,19 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    if (xs.isEmpty)
+      throw new java.util.NoSuchElementException
+    else
+      recursiveSearch(xs.head, xs.tail)
+  }
+
+  def recursiveSearch(maxValue: Int, list: List[Int]): Int = {
+    if (list.isEmpty)
+      maxValue
+    else if (list.head > maxValue)
+      recursiveSearch(list.head, list.tail)
+    else
+      recursiveSearch(maxValue, list.tail)
+  }
 }
